5815dfc6864d0d767eba0643c32cb2edb52d1674
maximilianosena <maximilianosena13@gmail.com>
2023-08-02 20:04:16 -0300

    Perfeccioné contador

--- script.js
+++ script.js
@@ -126,30 +126,34 @@ document.addEventListener("DOMContentLoaded", function () {
     //fecha limite en dia/mes/año
     let limite = new Date("9/3/2023 23:59:59")
 
-    function restar() {
-        let parrafo_dia = document.getElementById("dias");
-        // Calcular la diferencia en milisegundos
-        const diferenciaEnMilisegundos = limite - tiempo;
+    
+ // Calcular la diferencia en milisegundos
+ const diferenciaEnMilisegundos = limite - tiempo;
+ let parrafo_dia = document.getElementById("dias");
 
-         // Calcular los días, horas, minutos y segundos restantes
+ 
+        if (diferenciaEnMilisegundos<=0){
+let aviso= "Ya pasó la fecha de corte";
+parrafo_dia.textContent=aviso;
+        } else {
+            function restar() {
+                    // Calcular los días, horas, minutos y segundos restantes
 
          //1000 milisegundos= 1segundo, 60segundos=1min, 60min=1hr, 24hrs=1d
+         //devuelve cuantos días son esos milisegundos, y un resto
     const diasRestantes = Math.floor(diferenciaEnMilisegundos / (1000 * 60 * 60 * 24));
 
-    //con % toma los decimales
-    //(1000 * 60 * 60 * 24)) = total de milisegundos en un dia
-    //diferenciaEnMilisegundos % (1000 * 60 * 60 * 24))=calcula el residuo por cdad milisegundos en un dia
-    //((diferenciaEnMilisegundos % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))= la segunda división calcula
-    //cuantas horas son esos milisegundos
+   //diferenciaEnMilisegundos %  toma el resto de la división por el valor que le sigue
+   //la segunda división, especifica que valor retoma (1000ms*60s*60m)= Cdad hrs
+   //tomo el resto de la división hasta la unidad a averiguar, ej:
+   //minutosRestantes  % (1000ms *60s * 60m<- ) / (1000ms*60s <-cuantos minutos son)
+    
     const horasRestantes = Math.floor((diferenciaEnMilisegundos % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-    //diferenciaEnMilisegundos % (1000 * 60 * 60) calcula el residuo por cdad milisegundos en una hora
-    //((diferenciaEnMilisegundos % (1000 * 60 * 60)) / (1000 * 60))= la segunda division calcula cuantos minutos
-    //son esos milisegundos
+    
     const minutosRestantes = Math.floor((diferenciaEnMilisegundos % (1000 * 60 * 60)) / (1000 * 60));
 
-    //(diferenciaEnMilisegundos % (1000 * 60)) calcula el residuo por cdad milisegundos en un minuto
-    //((diferenciaEnMilisegundos % (1000 * 60)) / 1000) calcula cuantos segundos son esos milisegundos
+ 
     const segundosRestantes = Math.floor((diferenciaEnMilisegundos % (1000 * 60)) / 1000);
         
 
@@ -162,6 +166,7 @@ document.addEventListener("DOMContentLoaded", function () {
     restar();
 
     // Llamar a la función cada segundo para mantener la cuenta regresiva actualizada
+    //Valor tiempo está en ms
 setInterval(restar,1000);
-
+}
 })
